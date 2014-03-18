3969e7d8ede580fc976d30fce53c43cc967b2417
MDamien <damienlog@gmail.com>
2014-03-18 23:47:27 +0000

    Added a robot.txt

--- main.py
+++ main.py
@@ -2,21 +2,32 @@ from flask import Flask,render_template
 import core
 app = Flask(__name__)
 
+from flask import request
+
 @app.route("/map/")
 def siemap_index():
-	return sitemap("")
+    return sitemap("")
 
 @app.route("/map/<path:x>")
 def sitemap(x):
-	elrange = range(ord("a"),ord("z"))+range(ord("A"),ord("Z"))
-	return render_template("sitemap.html",title=x,links=[x+chr(c) for c in elrange])
+    if 'Wget' in request.user_agent.string:
+        return ""
+    elrange = range(ord("a"),ord("z"))+range(ord("A"),ord("Z"))
+    return render_template("sitemap.html",title=x,links=[x+chr(c) for c in elrange])
 
 @app.route("/")
 def index():
-    return generator("Hello")
+    return generator("Hello ")
+
+
+@app.route('/robots.txt')
+def robots():
+    return "User-Agent: *\nDisallow: /"
 
 @app.route("/<path:x>")
 def generator(x):
+    if 'Wget' in request.user_agent.string:
+        return ""
     return render_template("content.html",title=x,content=core.generate(x))
 
 if __name__ == "__main__":
